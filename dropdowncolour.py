60e56decf7d58e0694cf86d20d3dfb62a6818b4f
bilge kaan <bilgekagangurgen@msn.com>
2022-10-27 17:51:59 +0300

    denemeler yüklendi

---
+++ dropdowncolour.py
@@ -0,0 +1,24 @@
+import flet
+from flet import Dropdown, ElevatedButton, Page, Text, dropdown
+
+
+def main(page: Page):
+    def button_clicked(e):
+        output_text.value = f"Dropdown value is:  {color_dropdown.value}"
+        page.update()
+
+    output_text = Text()
+    submit_btn = ElevatedButton(text="Submit", on_click=button_clicked)
+    color_dropdown = Dropdown(
+        width=100, ##yatayda genişliği tanımlıyoruz.
+        options=[
+            dropdown.Option("Purple"),
+            dropdown.Option("Red"),
+            dropdown.Option("Green"),
+            dropdown.Option("Blue"),
+        ],
+    )
+    page.add(color_dropdown, submit_btn, output_text)
+
+flet.app(target=main, view=flet.WEB_BROWSER)
+
